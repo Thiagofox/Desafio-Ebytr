b0fcb75335809c349e6c5d4fba80233703eb1a1b
Thiagofox <thiagofox00@gmail.com>
2021-11-01 17:55:44 -0300

    foram feitas diversas mudaçãs no backend vizando funcinalida com o frontend adição do cors para a utilização dos fetchs no local host

--- backend/src/api/app.js
+++ backend/src/api/app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const toDoRouter = require('../routes/toDoRouter');
 
 const app = express();
+
+app.use(cors());
 app.use(bodyParser.json());
 
 const PORT = 3000;
